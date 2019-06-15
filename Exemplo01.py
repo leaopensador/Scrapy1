069a0de6892b380cca38b0db68105c0cc3ec63e6
figue <figueiredo..quanta@gmail.com>
2019-06-14 21:19:02 -0300

    Segundo Teste do Github

---
+++ Exemplo01.py
@@ -0,0 +1,5 @@
+#Teste do GitHub
+from urllib.request import urlopen
+#Retrieve HTML string from the URL
+html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
+print(html.read())
